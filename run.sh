8eb8ab44ca884d01cf00f7e05eb52e66202469f5
Patryk Kowalczyk <patrykkowalczyk565@gmail.com>
2026-02-04 21:57:43 +0100

    modified script

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ cd frontend
 npm run build
 
 cd ../backend
+source .venv/bin/activate
+pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:5304
